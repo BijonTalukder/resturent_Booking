9602a38006cf922e73dbc6aaabb02a2e0ba6b2b3
Bijon <bijontalukder1247@gmail.com>
2025-03-17 00:57:05 +0600

    user module done

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
+const UserController = require('../controllers/userController');
+const UserService = require('../services/User/userService');
 
 
 const router = express.Router();
 
 const prisma = new PrismaClient();
+const userService = new UserService(prisma)
+const userController= new UserController(userService)
+router.post("/user/register",async(req,res,next)=>{
+    userController.createUser(req,res,next)
 
+})
+
+router.post("/user/login",async(req,res,next)=>{
+    userController.login(req,res,next)
+})
+
+router.get("/user",async(req,res,next)=>{
+    userController.getAllUsers(req,res,next)
+})
+router.get("/user/:id",async(req,res,next)=>{
+    userController.getSingleUser(req,res,next)
+})
+router.put("/user/:id",async(req,res,next)=>{
+    userController.updateUser(req,res,next)
+})
 
 
 module.exports = router;
