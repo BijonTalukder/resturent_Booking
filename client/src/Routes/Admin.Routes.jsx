66b7e17d15363454a9064b70832275189adc3bbc
ahad30 <mohiminulislamahad@gmail.com>
2025-04-06 00:52:42 +0600

    adsad

--- client/src/Routes/Admin.Routes.jsx
+++ client/src/Routes/Admin.Routes.jsx
@@ -34,46 +34,39 @@ export const adminRoutes = [
     path: "/admin/profile",
     element: <EditAdminProfile/>,
   },
-  // {
-  //   path: "/admin/changePassword",
-  //   element: <AdminChangePassword></AdminChangePassword>,
-  // },
-
-  
-
   {
-    label: "Products Management",
+    label: "Hotel Management",
     icon: <AiFillBoxPlot size={20} />,
     children: [
+      // {
+      //   path: "categories",
+      //   label: "Category",
+      //   element: <Category></Category>,
+      //   permissionName: "view category",
+      // },
+      // {
+      //   path: "brands",
+      //   label: "Brands",
+      //   element: <Brand></Brand>,
+      //   permissionName: "view brand",
+      // },
+      // {
+      //   path: "attributes",
+      //   label: "Attribute",
+      //   element: <Attribute></Attribute>,
+      //   permissionName: "view attribute",
+      // },
       {
-        path: "categories",
-        label: "Category",
-        element: <Category></Category>,
-        permissionName: "view category",
-      },
-      {
-        path: "brands",
-        label: "Brands",
-        element: <Brand></Brand>,
-        permissionName: "view brand",
-      },
-      {
-        path: "attributes",
-        label: "Attribute",
-        element: <Attribute></Attribute>,
-        permissionName: "view attribute",
-      },
-      {
-        path: "products",
-        label: "Products",
+        path: "hotels",
+        label: "Hotels",
         element: <Product></Product>,
         permissionName: "view product",
       },
     ],
   },
   {
-    path: "orders",
-    label: "Orders",
+    path: "bookings",
+    label: "Bookings",
     element: <Orders />,
     icon: <CiShop size={20}/>,
   },
@@ -83,30 +76,6 @@ export const adminRoutes = [
     element: <Users />,
     icon: <FaUsers size={20}/>,
   },
-
-
-  // {
-  //   label: "E-commerce",
-  //   icon: (
-  //     <MdOutlineShoppingCartCheckout size={20}></MdOutlineShoppingCartCheckout>
-  //   ),
-  //   children: [
-  //     {
-  //       path: "orders",
-  //       label: "Orders",
-  //       element: <Orders />,
-  //       permissionName: "view customer",
-  //     },
-  //     {
-  //       path: "customers",
-  //       label: "Customers",
-  //       element: <Customers />,
-  //       permissionName: "view customer",
-  //     },
-  //   ],
-  // },
-
-
   {
     path: "add-product",
     element: <AddProduct></AddProduct>,
@@ -146,60 +115,4 @@ export const adminRoutes = [
     icon: <MdContacts  size={20}/>,
     permissionName: "view contact",
   },
-
-  // {
-  //   label: "Package Management",
-  //   icon: <TfiPackage size={20}></TfiPackage>,
-  //   children: [
-  //     {
-  //       path: "packages",
-  //       label: "New product packages",
-  //       element: <Packages></Packages>,
-  //       permissionName: "view package",
-  //     },
-  //     {
-  //       path: "exist-product-packages",
-  //       label: "Exist product packages",
-  //       element: <ExistProductPackages></ExistProductPackages>,
-  //       permissionName: "view package",
-  //     },
-  //   ],
-  // },
-
-  // {
-  //   label: "User Management",
-  //   icon: <LuUserCog2 size={20}></LuUserCog2>,
-  //   children: [
-  //     {
-  //       path: "permissions",
-  //       label: "Permissions",
-  //       element: <Permissions></Permissions>,
-  //       permissionName: "view permissions",
-  //     },
-  //     {
-  //       path: "roles",
-  //       label: "Roles",
-  //       element: <Roles></Roles>,
-  //       permissionName: "view role",
-  //     },
-  //     {
-  //       path: "users",
-  //       label: "Users",
-  //       element: <Users></Users>,
-  //       permissionName: "view user",
-  //     },
-  //   ],
-  // },
-  // {
-  //   label: "Configuration",
-  //   icon: <IoConstructOutline size={20}></IoConstructOutline>,
-  //   children: [
-  //     {
-  //       path: "sliders",
-  //       label: "Sliders",
-  //       element: <Slider></Slider>,
-  //       permissionName: "view slider",
-  //     },
-  //   ],
-  // },
 ];
