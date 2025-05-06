d7937e455f8a81b2f1e9c5e71c72b374656a1993
BijonTalukder <bijontalukder1247@gmail.com>
2025-05-06 11:44:47 +0600

    added new search system

--- client/src/Pages/District/District.jsx
+++ client/src/Pages/District/District.jsx
@@ -10,7 +10,7 @@ const District = () => {
 
   useEffect(() => {
     if (districtsData) {
-      setDistricts(districtsData.data); // Assuming the API returns { data: [...] }
+      setDistricts(districtsData.data);
     }
   }, [districtsData]);
 
@@ -32,6 +32,23 @@ const District = () => {
         <LeftCircleFilled className="ms-2 mt-2 text-2xl" />
       </Link>
       <div className="w-full max-w-md mx-auto bg-white min-h-screen p-4 space-y-6 mb-16">
+       
+      <div className="flex items-center justify-between border-b pb-2">
+        <button
+          // onClick={goPrevious}
+          className="text-sm px-2 py-1 bg-gray-200 rounded disabled:opacity-50"
+        >
+          Prev
+        </button>
+        <h2 className="text-lg font-semibold"></h2>
+        <button
+          // onClick={goNext}
+          className="text-sm px-2 py-1 bg-gray-200 rounded disabled:opacity-50"
+        >
+          Next
+        </button>
+      </div>
+       
         <div className="space-y-2">
           <h3 className="text-md font-medium">Select District</h3>
           <ul className="space-y-2">
@@ -40,7 +57,7 @@ const District = () => {
                 <li
                   className="bg-blue-100 px-4 py-3 rounded shadow text-center cursor-pointer hover:bg-blue-200 mb-4"
                 >
-                  {district.name} {/* Adjust based on your district object structure */}
+                  {district.name} 
                 </li>
               </Link>
             ))}
